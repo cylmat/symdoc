39c1a53971fc247cf742cfc1bec8d13ce5bd5ad9
cylmat <cyrilmatte.pro@gmail.com>
2021-12-16 16:34:37 +0000

    rename

--- src/Domain/Manager/AllManager.php
+++ src/Domain/Manager/AllManager.php
@@ -14,11 +14,14 @@ use Symfony\Component\Form\Extension\Core\Type\DateTimeType;
 use Symfony\Component\Form\Extension\Core\Type\FormType;
 use Symfony\Component\Form\FormEvents;
 use Symfony\Component\Form\FormTypeInterface;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpKernel\DataCollector\DataCollectorInterface;
 use Symfony\Component\HttpKernel\Event\KernelEvent;
 use Symfony\Component\HttpKernel\Kernel;
 use Symfony\Component\Security\Core\Authentication\Provider\AuthenticationProviderInterface;
+use Symfony\Component\Security\Core\Authorization\AccessDecisionManager;
 use Symfony\Component\Security\Core\Security;
+use Symfony\Component\Security\Core\User\UserChecker;
 use Symfony\Component\Serializer\Mapping\Factory\ClassMetadataFactory;
 use Symfony\Component\Serializer\Mapping\Loader\AnnotationLoader;
 use Symfony\Component\Serializer\Serializer;
@@ -84,7 +87,7 @@ final class AllManager implements ManagerInterface
         Finder::class;
         InputArgument::class;
         Kernel::class;
-        Security::class;
+        Response::class;
         Serializer::class;
         VarDumper::class;
         Yaml::class;
@@ -103,5 +106,12 @@ final class AllManager implements ManagerInterface
     {
         ConfigCacheFactory::class;
         ClassMetadataFactory::class;
+    }
+    
+    public function security()
+    {
+        AccessDecisionManager::class;
+        Security::class;
+        UserChecker::class;
     }
 }
