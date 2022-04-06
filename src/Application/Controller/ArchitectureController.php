e25943ca1f7e328f7882cee14f142c2e72b22237
cylmat <cyrilmatte.pro@gmail.com>
2022-04-06 15:39:28 +0000

    httpkernel info

--- src/Application/Controller/ArchitectureController.php
+++ src/Application/Controller/ArchitectureController.php
@@ -12,6 +12,7 @@ use App\Domain\Manager\HeaderManager;
 use DateTime;
 use DateTimeZone;
 use Psr\Log\LoggerInterface;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\{ParamConverter, Template};
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\DependencyInjection\ServiceLocator;
 use Symfony\Component\EventDispatcher\EventDispatcher;
@@ -68,9 +69,32 @@ final class ArchitectureController extends AbstractController
     }
 
     /**
-     * @Route("/http-kernel")
-     *
+     * @Route("/httpkernel")
+     */
+    /*
      * composer require symfony/http-kernel
+     * bin/console debug:event-dispatcher kernel.controller_arguments
+     * @see https://symfony.com/doc/5.0/reference/events.html
+     *
+     * HttpKernel::handle(Request $request, int $type = HttpKernelInterface::MASTER_REQUEST, bool $catch = true)
+     *
+     * kernel.request: Add more information to Request, initialize the system, return Response (e.g. add security layer)
+     *  -> RouterListener
+     * kernel.controller: Initialize things or change the controller just before the controller is executed.
+     *  -> ControllerResolverInterface (e.g. FooBundle:Default:index format)
+     *  -> collecting profiler data
+     * kernel.controller_arguments
+     *  -> @ParamConverter
+     *  -> ArgumentResolverInterface
+     * kernel.view: Transform a non-Response return value from a controller into a Response
+     *  -> propagation is stopped
+     *  -> @Template
+     * kernel.response: Modify the Response object just before it is sent
+     *  ->  listener modify the Response, modifying headers, adding cookies, injecting some JavaScript...
+     * kernel.finish_request: Reset the global state of the application
+     * kernel.terminate: To perform some "heavy" action after the response has been streamed to the user
+     *  -> TerminableInterface
+     * kernel.exception
      */
     public function httpkernel(Request $request, HeaderManager $headerManager): Response
     {
