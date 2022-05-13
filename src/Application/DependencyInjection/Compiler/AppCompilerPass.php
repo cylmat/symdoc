cfd19c407aeca47db7516cd5ade4de4abbd55ceb
cylmat <cyrilmatte.pro@gmail.com>
2022-05-13 14:50:40 +0000

    fix

--- src/Application/DependencyInjection/Compiler/AppCompilerPass.php
+++ src/Application/DependencyInjection/Compiler/AppCompilerPass.php
@@ -18,20 +18,20 @@ class AppCompilerPass implements CompilerPassInterface
             // ->set Property, Factory, Class, Argument, MethodCall, Autoconfigured, Tags, Shared...
         $twigDefinition->setPublic(false);
 
-        $container->setParameter('twig.form.resources', '8');
-        $container->setParameter('twig.default_path', 9);
+        $container->setParameter('twig.form.resources.sample', '8');
+        $container->setParameter('twig.default_path.sample', 9);
 
         // long way...
-        $definition = new Definition('twig.form.resources', [
+        $definition = new Definition('twig.form.resources.sample', [
             'constructor',
             'arguments',
             new Reference('doctrine'), // reference to an existing service
         ]);
         $args = $definition->getArguments();
-        $container->setDefinition('pass.twig.form.resources', $definition);
+        $container->setDefinition('pass.twig.form.resources.sample', $definition);
 
         // ...or shortcut
-        $container->register('pass.twig.form.resources', 'twig.form.resources');
+        $container->register('pass.twig.form.resources.sample', 'twig.form.resources.sample');
 
         // TAGS
         $testTaggedServices = $container->findTaggedServiceIds('app.test');
